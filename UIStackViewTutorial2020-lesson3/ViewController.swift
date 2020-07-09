df8044ad54acf807740ba2934a94d80385bf8de9
Michael Tamsil <michaeltamsil@gmail.com>
2020-07-09 10:03:27 +0700

    initial commit

---
+++ UIStackViewTutorial2020-lesson3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIStackViewTutorial2020-lesson3
+//
+//  Created by michael tamsil on 08/07/20.
+//  Copyright © 2020 michael tamsil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
